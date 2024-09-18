fa7692da5ad3d1295866c9b8c4431659773df2b9
ProperNovice <ProperNovice@users.noreply.github.com>
2024-09-18 16:43:50 +0300

    q

---
+++ src/cards/CardMachinePairs.java
@@ -0,0 +1,11 @@
+package cards;
+
+import enums.EMachineStage;
+
+public class CardMachinePairs extends CardMachine {
+
+	public CardMachinePairs(EMachineStage eMachineStage) {
+		super(eMachineStage);
+	}
+
+}
--- src/gameStates/JUnit.java
+++ src/gameStates/JUnit.java
@@ -1,15 +1,11 @@
 package gameStates;
 
-import cards.CardMachineLessThan;
-import enums.EMachineStage;
 import gameStatesDefault.GameState;
 
 public class JUnit extends GameState {
 
 	@Override
 	public void execute() {
-
-		new CardMachineLessThan(EMachineStage.II, 6);
 
 	}
 
